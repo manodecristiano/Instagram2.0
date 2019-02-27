5307e70d7431f39e82f33c1b7d71cbc607a3e799
Cristian Carreño Navarro <cristiancarrenonavarro@cristians-imac.local>
2019-02-27 01:51:41 -0800

    Initial Commit

---
+++ Instagram2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram2.0
+//
+//  Created by Cristian Carreño Navarro on 27/02/2019.
+//  Copyright © 2019 Cristian Carreño Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
